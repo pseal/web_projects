2b08c343c714f17b0360937079f5300d4da61789
pseal <33536426+pseal@users.noreply.github.com>
2025-11-20 17:27:53 +0200

    Add files via upload

---
+++ python201_projects.py
@@ -0,0 +1,17 @@
+import requests
+
+while True:
+    pokemon = input("Which pokemon characters you want to find?")
+    pokemon = pokemon.lower()
+
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+
+    req = requests.get(url)
+    if req.status_code == 200:
+        data = req.json()
+        print(f"Name is {data['name']}")
+        print("Abilities:")
+        for ability in data['abilities']:
+            print(ability['ability']['name'])
+    else:
+        print("Pokemon character not found")
